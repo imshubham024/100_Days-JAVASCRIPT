a13c46b5d193b0b59c9f1b10b6e21afe68c74a2a
Shubham gupta <133352320+imshubham024@users.noreply.github.com>
2024-05-25 15:16:57 +0530

    Day 3
    Day 3

---
+++ day 3/try_catch.js
@@ -0,0 +1,16 @@
+
+  //Syntax
+
+// try {
+//     Block of code to try
+//   }
+//   catch(err) {
+//     Block of code to handle errors
+//   }
+
+try{
+    const result=5/0;
+}
+catch(err) {
+  console.log(err);
+}
