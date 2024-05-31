e2134a52e7c25c95fbcdd4cc5f87f59d1f9bfd7d
Shubham gupta <133352320+imshubham024@users.noreply.github.com>
2024-05-31 23:59:28 +0530

    Update sumofNo.js

--- Day 9/sumofNo.js
+++ Day 9/sumofNo.js
@@ -1,9 +1,10 @@
-const sum = (arr) => {
-    let ans=arr.reduce((acc,curr)=>{
-        return{
-            let reserve=curr%10,
-        }
-    })
+const sum = (n) => {
+    let sum = 0;
+    while (n != 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return sum;
 }
 
-console.log(sum(1234)); // Output: 10
+console.log(sum(1234));  // Output: 10
