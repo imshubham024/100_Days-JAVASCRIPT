0b1221424ef1708f42fd4efa72841c7eb1c7ded3
Shubham gupta <133352320+imshubham024@users.noreply.github.com>
2024-06-12 00:06:17 +0530

    Update reverseString.js

--- Day 14/reverseString.js
+++ Day 14/reverseString.js
@@ -1,9 +1,20 @@
-const reverse=(str)=>{
-    let newstr="";
-    for(i=str.length;i>=0;i--){
-        newstr+=str[i];
+const reverse = (str) => {
+    let newstr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newstr += str[i];
     }
     return newstr;
 }
 
-console.log(reverse("shubham"))
+console.log(reverse("shubham")); // Outputs: mahbuhs
+
+
+let reverse2=(str)=>{
+    let newstr=[];
+    str=str.split("").map((val)=>{
+        newstr.unshift(val);
+    })
+    return newstr.join("");
+}
+
+console.log(reverse2("shubham")); // Outputs:mahbuhs
